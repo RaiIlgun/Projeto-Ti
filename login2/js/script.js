f15aa07b4e8178be4c4d316fcaab320a91c9d170
RaiIlgun <renanzinho.queiroz@gmail.com>
2022-07-05 22:21:58 -0300

    tela de registro e começo de inteligencia js

---
+++ login2/js/script.js
@@ -0,0 +1,31 @@
+class Validator {
+
+    constructor() {
+        this.validations = [
+            
+        ]
+    }
+
+    validate(form) {
+
+        let inputs = form.getElementsbyTagName('input');
+        console.log(inputs);
+        let inputsArray = [...inputs];
+        console.log(inputsArray);
+
+    }
+
+}
+
+let form = document.getElementById("regform");
+let submit = document.getElementById("btn-reg");
+let validator = new Validator();
+
+submit.addEventListener('click', function(e) {
+
+    e.preventDefault();
+    validator.validate(form);
+
+    console.log("test");
+
+})
