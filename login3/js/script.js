00df0686bee05ea8ed8797bcd9f62dc2eb8996df
RaiIlgun <renan15777@live.com>
2022-07-14 16:53:05 -0300

    corrteção cpf teste

--- login3/js/script.js
+++ login3/js/script.js
@@ -1,71 +1,21 @@
-class Validator {
+function ValidaCPF(){   
 
-    constructor() {
-        this.validations = [
-            "data-min-length",
-        ]
+    var ao_cpf=document.forms.caixa.ao_cpf.value; 
+    var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/;     
+    if (cpfValido.test(ao_cpf) == false)    { 
+         
+       ao_cpf = ao_cpf.replace( /\D/g , ""); //Remove tudo o que não é dígito
+                
+       if (ao_cpf.length==11){
+          ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+          ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+          //de novo (para o segundo bloco de números)
+          ao_cpf = ao_cpf.replace( /(\d{3})(\d{1,2})$/ , "$1-$2"); //Coloca um hífen entre o terceiro e o quarto dígitos
+                    
+          var valorValido = document.getElementById("ao_cpf").value = ao_cpf;
+        }else{
+          console.log("CPF invalido");
+        }
+            
     }
-
-    validate(form) {
-
-        let inputs = form.getElementsByTagName('input');
-        let inputsArray = [...inputs];
-        // loop e validação
-        inputsArray.forEach(function(input){
-
-            for(let i = 0; this.validations.length > i; i++){
-                if(input.getAttribute(this.validations[i]) != null){
-
-                    let method = this.validations[i].replace('data-', '').replace('-', '');
-
-                    let value = input.getAttribute(this.validations[i]);
-
-                    this[method](input, value);
-
-
-
-                }
-            }
-
-        }, this);
-
     }
-
-    minlength(input, minValue){
-
-       let inputLength = input.value.length;
-       let errorMessage = `O campo deve conter pelomenos ${minValue} caracteres.`;
-       if(inputLength < minValue){
-        this.printMessage(input, errorMessage);
-       }
-
-    }
-
-    printMessage(input, msg){
-
-        let template = document.querySelector('.error-validation').cloneNode(true);
-
-        template.textContent = msg;
-
-        let inputParent = input.parentNode;
-
-        template.classList.remove('template');
-
-        inputParent.appendChild(template);
-
-
-    }
-
-}
-
-let form = document.getElementById("regform");
-let submit = document.getElementById("btn-reg");
-let validator = new Validator();
-
-submit.addEventListener('click', function(e) {
-
-    e.preventDefault();
-    validator.validate(form);
-
-
-})
