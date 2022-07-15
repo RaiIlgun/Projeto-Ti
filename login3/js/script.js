47c2cd409b36025a32caa444f4b24bd9c2825d89
RaiIlgun <renan15777@live.com>
2022-07-15 17:34:47 -0300

    nova branch validarCPF

--- login3/js/script.js
+++ login3/js/script.js
@@ -1,21 +1,94 @@
-function ValidaCPF(){   
+var ao_cpf = document.forms.caixa.ao_cpf.value; 
 
-    var ao_cpf=document.forms.caixa.ao_cpf.value; 
-    var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/;     
-    if (cpfValido.test(ao_cpf) == false)    { 
-         
-       ao_cpf = ao_cpf.replace( /\D/g , ""); //Remove tudo o que não é dígito
-                
-       if (ao_cpf.length==11){
-          ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
-          ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
-          //de novo (para o segundo bloco de números)
-          ao_cpf = ao_cpf.replace( /(\d{3})(\d{1,2})$/ , "$1-$2"); //Coloca um hífen entre o terceiro e o quarto dígitos
-                    
-          var valorValido = document.getElementById("ao_cpf").value = ao_cpf;
-        }else{
-          console.log("CPF invalido");
-        }
-            
-    }
-    }
+function validarpD(cpf) {
+  let sum = 0;
+  for(let i = 0; i < 9; i++){
+    sum += cpf[i] * (10 - i);
+  }
+  const resto = (sum * 10) % 11;
+  if(resto < 10) {
+    return cpf[9] == resto
+  }
+  return cpf[9] == 0
+ return false;
+}
+function validarsD(cpf) {
+  let sum = 0;
+  for(let i = 0; i < 10; i++){
+    sum += cpf[i] * (11 - i);
+  }
+  const resto = (sum * 10) % 11;
+  if(resto < 10) {
+    return cpf[10] == resto
+  }
+  return cpf[10] == 0
+ return false;
+}
+
+
+
+
+
+function validacaoCpf(cpf) {
+  /*if(!cpfrepetido(cpf)){
+    return false;
+  }*/
+  if(cpf.length != 11) {
+    return false;
+  }
+  if(!validarpD(cpf)) {
+    return false;
+  }
+  if(!validarsD(cpf)) {
+    return false;
+  }
+return true;
+}
+
+const cpf = ao_cpf.split('').map((e) => parseInt(e));
+const cpfok = validacaoCpf(cpf);
+
+console.log(cpfok);
+
+    
+    function AnaliseCPF(){
+
+      var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/;     
+      if (cpfValido.test(ao_cpf) == false)    { 
+
+          console.log(cpf);
+          const primeiro = cpf[0];
+          let diferente = false;
+          for(let i = 1; 1 < cpf.length; i++) {
+            if(cpf[i] != primeiro) {
+              diferente = true;
+            }
+          }
+          return diferente;
+      
+         ao_cpf = ao_cpf.replace( /\D/g , ""); //Remove tudo o que não é dígito
+                  
+         if (ao_cpf.length==11){
+            ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+            ao_cpf = ao_cpf.replace( /(\d{3})(\d)/ , "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+            //de novo (para o segundo bloco de números)
+            ao_cpf = ao_cpf.replace( /(\d{3})(\d{1,2})$/ , "$1-$2"); //Coloca um hífen entre o terceiro e o quarto dígitos
+            document.getElementById("ao_cpf").style.backgroundColor = "black";
+                      
+            var valorValido = document.getElementById("ao_cpf").value = ao_cpf;
+            console.log("CPF válido");
+          }else{
+            console.log("CPF invalido");
+            document.getElementById("ao_cpf").style.backgroundColor = "red";
+          }
+              
+      }   
+   }
+function ValidarCPF() {
+
+  if(AnaliseCPF(ao_cpf)){
+    return = true
+    
+  }
+
+}
